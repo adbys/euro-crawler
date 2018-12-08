4156fc5806a01e643bf78750e4117e9d97fbdf3a
Adbys José Vascocnelos de Andrade <adbys.andrade@ccc.ufcg.edu.br>
2018-12-08 16:01:05 -0300

    merge

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 8080
 const crawler = require('./crawler.js');
+var cors = require('cors');
+
+app.use(cors());
 
 var url = require('url');
 
